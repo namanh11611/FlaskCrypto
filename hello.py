715d9a3a639b33df6e050334e2ee917f3d3942d3
Nam Anh Nguyen <namanh11611@gmail.com>
2021-01-14 01:51:33 +0700

    Return JSON

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,3 +8,37 @@ def index():
 @app.route('/welcome')
 def welcome():
     return 'Welcome page'
+
+@app.route('/me')
+def me():
+    return jsonify(
+        name="Nam Anh",
+        age=24
+    )
+
+@app.route("/users")
+def users_api():
+    users = get_all_users()
+    return jsonify(users)
+
+def get_all_users():
+    # return [Person("Nam Anh", 25), Person("Quynh Hoa", 25), Person("Viet Ha", 21)]
+    return [
+        {
+            "name": "Nam Anh",
+            "age": 25
+        },
+        {
+            "name": "Nam Anh",
+            "age": 25
+        },
+        {
+            "name": "Nam Anh",
+            "age": 25
+        }
+    ]
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
